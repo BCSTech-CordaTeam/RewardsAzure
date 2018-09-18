bcfe18d96c00cce062c2bfb680d876b13086dd94
Nep <harrison.mckenzie@bcstechnology.com.au>
2018-09-19 09:47:09 +1000

    Nicified script.

--- setup.sh
+++ setup.sh
@@ -7,6 +7,9 @@ POSTGRES_DBNAME=$3
 POSTGRES_LOGIN=$4
 POSTGRES_PASS=$5
 PASS=$6 # Not supplied by user
+USER=$7
+
+FILE="rewards-0.0.3-DEV"
 
 ### Download/Install dependencies
 apt-get update -y
@@ -15,13 +18,16 @@ apt-get install default-jre -y
 # 7zip
 apt-get install p7zip-full -y
 
+### Move to a nice location
+mkdir /home/$USER/rewards && cd "$_"
+
 ### Download the jar
-wget https://github.com/BCSTech-CordaTeam/RewardsAzure/blob/master/rewards-0.0.3-DEV.7z?raw=true
-mv rewards-0.0.3-DEV.7z?raw=true rewards-0.0.3-DEV.7z
+wget https://github.com/BCSTech-CordaTeam/RewardsAzure/blob/master/$FILE.7z?raw=true
+mv $FILE.7z?raw=true $FILE.7z
 
 ### Extract it
 # password: fh2fhq8fhy48hSDF43Gfde
-7z x rewards-0.0.3-DEV.7z -p$PASS
+7z x $FILE.7z -p$PASS
 
 ### Generate the application.properties file
 mkdir ./WEB-INF/classes -p
@@ -47,7 +53,29 @@ spring.datasource.username=$POSTGRES_LOGIN
 spring.datasource.password=$POSTGRES_PASS" > ./WEB-INF/classes/application.properties
 
 ### Move application.properties into the WAR
-7z u rewards-0.0.3-DEV.war ./WEB-INF
+7z u $FILE.war ./WEB-INF
+
+### Cleanup
+rm WEB-INF -r -f
+
+### Generate script for starting server
+echo "
+#!/bin/sh
+java -jar ./$FILE.war" > start.sh
 
 ### Run the jar
-nohup java -jar ./rewards-0.0.3-DEV.war & 
+nohup ./start.sh & 
+
+### Self destruct
+# Get path
+# Source: https://stackoverflow.com/questions/630372/determine-the-path-of-the-executing-bash-script
+MY_PATH="`dirname \"$0\"`"              # relative
+MY_PATH="`( cd \"$MY_PATH\" && pwd )`"  # absolutized and normalized
+if [ -z "$MY_PATH" ] ; then
+  # error; for some reason, the path is not accessible
+  # to the script (e.g. permissions re-evaled after suid)
+  exit 1  # fail
+fi
+
+rm $MY_PATH/${0##*/}
+
